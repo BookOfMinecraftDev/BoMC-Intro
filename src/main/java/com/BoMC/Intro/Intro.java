ca6c871ced56d9a11d758d63076adb93b4dd33b1
Adam Taylor <afranklint16@gmail.com>
2021-07-11 11:05:47 -0600

    Fixed IllegalStateException
    The plugin previously only stored one list with one of each title as a BukkitRunnable, and scheduling the same instance twice causes problems. Fixed this by adding a clone function in Title that simply returns a new instance of the runnable with the same settings, and then re-creates the entire list each time the display is needed. This isn't the best solution because it relies on the garbage collector but it is the simplest and fastest fix to this problem without rewriting the code. Also, my eclipse has a formatter, and so most of the little changes are from that.

--- src/main/java/com/BoMC/Intro/Intro.java
+++ src/main/java/com/BoMC/Intro/Intro.java
@@ -21,75 +21,81 @@ public class Intro extends JavaPlugin {
 
 	public static List<Title> titles;
 	public static List<UUID> seen;
-	
+
 	public static int totalLength;
 	public static boolean blindness;
-	
+
 	public static FileConfiguration intro;
-	
+
 	public static FileConfiguration config() {
-		
+
 		return Bukkit.getPluginManager().getPlugin("BoMC-Intro").getConfig();
-		
+
 	}
 
 	@Override
 	public void onEnable() {
-		
+
 		// Save intro.yml
 		saveResource("intro.yml", false);
-		
+
 		intro = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "intro.yml"));
-		
+
 		// Config data
 		saveDefaultConfig();
 		IO.load(intro);
 
-		/*Stream<String> file = new BufferedReader(new InputStreamReader(getResource("intro.txt"))).lines();
-		paragraphs = file.collect(Collectors.toList());
-
-		int i = 1;
-		for (String s : paragraphs) {
-			
-			getLogger().info("Message " + i + ": '" + s + "'");
-			
-		}*/
+		/*
+		 * Stream<String> file = new BufferedReader(new
+		 * InputStreamReader(getResource("intro.txt"))).lines(); paragraphs =
+		 * file.collect(Collectors.toList());
+		 *
+		 * int i = 1; for (String s : paragraphs) {
+		 *
+		 * getLogger().info("Message " + i + ": '" + s + "'");
+		 *
+		 * }
+		 */
 
 		// Register listeners and commands
 		getServer().getPluginManager().registerEvents(new LoginListener(), this);
 		getCommand("intro").setExecutor(new IntroCommand());
-		
+
 	}
 
 	@Override
 	public void onDisable() {
-		
+
 		IO.save();
 		saveConfig();
-		
+
 	}
 
 	public static void display(Player player) {
-		
-		if(blindness) {
-			
-			player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, totalLength, 3));
-			
+
+		for (int i = 0; i < titles.size(); i++) {
+			titles.set(i, titles.get(i).clone());
 		}
-		
+
+		if (blindness) {
+
+			player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, totalLength, 3));
+
+		}
+
 		int timeIncrement = 10;
-		
-		for(Title t : titles) {
-			
+
+		for (Title t : titles) {
+
 			Title run = t;
-			
+
 			run.setPlayer(player);
 			run.runTaskLater(Bukkit.getPluginManager().getPlugin("BoMC-Intro"), timeIncrement);
-			
+
 			timeIncrement += t.getTotalLength();
-			
+
 		}
-		
+
 	}
-	
+
 }
--- src/main/java/com/BoMC/Intro/core/Title.java
+++ src/main/java/com/BoMC/Intro/core/Title.java
@@ -5,7 +5,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import net.md_5.bungee.api.ChatColor;
 
-public class Title extends BukkitRunnable{
+public class Title extends BukkitRunnable {
 
 	private Player player;
 	private String title;
@@ -18,60 +18,82 @@ public class Title extends BukkitRunnable{
 	private int totalLength;
 
 	public Title(String title, String sub, ChatColor titleColor, ChatColor subColor, int time) {
-		
+
 		this.title = "";
 		this.sub = "";
 		this.titleColor = ChatColor.WHITE;
 		this.subColor = ChatColor.WHITE;
-		
-		if(title != null) this.title = title;
-		if(sub != null) this.sub = sub;
-		if(titleColor != null) this.titleColor = titleColor;
-		if(subColor != null) this.subColor = subColor;
-		this.fadeIn = 0;
+
+		if (title != null) {
+			this.title = title;
+		}
+		if (sub != null) {
+			this.sub = sub;
+		}
+		if (titleColor != null) {
+			this.titleColor = titleColor;
+		}
+		if (subColor != null) {
+			this.subColor = subColor;
+		}
+		fadeIn = 0;
 		this.time = time;
-		this.fadeOut = 0;
-		
+		fadeOut = 0;
+
 		totalLength = time;
-		
+
 	}
-	
-	public Title(String title, String sub, ChatColor titleColor, ChatColor subColor, int fadeIn, int time, int fadeOut) {
-		
+
+	public Title(String title, String sub, ChatColor titleColor, ChatColor subColor, int fadeIn, int time,
+			int fadeOut) {
+
 		this.title = "";
 		this.sub = "";
 		this.titleColor = ChatColor.WHITE;
 		this.subColor = ChatColor.WHITE;
-		
-		if(title != null) this.title = title;
-		if(sub != null) this.sub = sub;
-		if(titleColor != null) this.titleColor = titleColor;
-		if(subColor != null) this.subColor = subColor;
+
+		if (title != null) {
+			this.title = title;
+		}
+		if (sub != null) {
+			this.sub = sub;
+		}
+		if (titleColor != null) {
+			this.titleColor = titleColor;
+		}
+		if (subColor != null) {
+			this.subColor = subColor;
+		}
 		this.fadeIn = fadeIn;
 		this.time = time;
 		this.fadeOut = fadeOut;
-		
-		totalLength = fadeIn  + time + fadeOut;
-		
+
+		totalLength = fadeIn + time + fadeOut;
+
 	}
-	
+
 	public void setPlayer(Player player) {
-		
+
 		this.player = player;
-		
+
 	}
 
 	public int getTotalLength() {
-		
+
 		return totalLength;
-		
+
 	}
 
 	@Override
 	public void run() {
 
 		player.sendTitle(titleColor + title, subColor + sub, fadeIn, time, fadeOut);
-		
+
+	}
+
+	@Override
+	public Title clone() {
+		return new Title(title, sub, titleColor, subColor, fadeIn, time, fadeOut);
 	}
 
 }
